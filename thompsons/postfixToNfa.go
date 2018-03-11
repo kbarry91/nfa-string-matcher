ae4dd95b71adb945f5c0e5476b2dbb31818ae581
kbarry91 <g00339811@gmit.ie>
2018-03-11 18:16:16 +0000

    Added thompsons package

---
+++ thompsons/postfixToNfa.go
@@ -0,0 +1,11 @@
+/*
+Package thompsons is used to implement Thompsons construction of a postfix regular expresion to a Non Determistic Finite Automota
+*/
+package thompsons
+
+/*
+PostToNfa function takes in a postfix regular expression to construct a nfa
+*/
+func PostToNfa(postfix string) {
+
+}
