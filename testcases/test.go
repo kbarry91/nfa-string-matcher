a14120d09f5968e851b5052f7ab07d0bc3e3ee8b
kbarry91 <g00339811@gmit.ie>
2018-04-07 21:16:16 +0100

    added test for string input

--- testcases/test.go
+++ testcases/test.go
@@ -13,13 +13,16 @@ func TestMenu() {
 	returnToMain := false
 	for !returnToMain {
 		utils.StringPrinter("\n" + testMenu)
-		progOption := utils.UserInput("1. Test String Concatenate \n2. Test Infix to Pofix \n3. Return To Main")
+		progOption := utils.UserInput("1. Test String Concatenate \n2. Test Infix to Pofix \n3. Test String Input \n4. Return To Main")
 		switch progOption {
 		case "1":
 			TestConcat()
 		case "2":
 			utils.StringPrinter("test infix to pofix Selected ....")
 		case "3":
+			utils.StringPrinter("Test input Selected ....")
+			TestInput()
+		case "4":
 			utils.StringPrinter("Returning To main menu  ....")
 			returnToMain = true
 		default:
@@ -34,9 +37,20 @@ TestConcat tests the ConcatenateInfix method
 */
 func TestConcat() {
 	utils.StringPrinter("Test Concatenate Selected ....")
-	// DEBUG : userInput
-	// TEST  : test input by entering a string
+	// DEBUG : ConcatenateInfix
+	// TEST  : (ab)*(ab) should return (a.b)*.(a.b)
 	// RESULT: Passedtest
 	var testCat = utils.UserInput("DEBUG Concat: \n \tEnter String to Test: ")
 	utils.StringPrinter("DEBUG Concat: \n \t" + testCat + " adapts to " + shuntingYard.ConcatenateInfix(testCat))
+}
+
+/*
+TestInput testsUserInput Method
+*/
+func TestInput() {
+	// DEBUG : userInput
+	// TEST  : test input by entering a string
+	// RESULT: Passedtest
+	var testString = utils.UserInput("DEBUG INPUT: \n \tEnter String to Test: ")
+	utils.StringPrinter("DEBUG INPUT: \n \t " + testString)
 }
